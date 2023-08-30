004a5604aaf73c79afcadcac5adbcaa6734f0c9f
artursuskevits <120181393+artursuskevits@users.noreply.github.com>
2023-08-30 11:53:37 +0300

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -6,3 +6,16 @@ INSERT INTO room_type (description, max_capacity)
 VALUES ('cool room',3);
  
 select * from room_type
+
+Insert Into  guest (first_name,last_name,member_since)
+ Values ('Vlad','Andreev','2022-12-24');
+ Insert Into  guest (first_name,last_name,member_since)
+ Values ('Artem','Maksimov','2022-12-24');
+ Insert Into  guest (first_name,last_name,member_since)
+ Values ('Ignat','iiOO','2022-12-24');
+ Insert Into  guest (first_name,last_name,member_since)
+ Values ('Liza','AHAHAHAh','2022-12-24');
+ Insert Into  guest (first_name,last_name,member_since)
+ Values ('Dylan','Gdeon','2022-12-24');
+
+ select * from guest
