01af9553bdfb5f5b585be84493c87abe70bd0d9a
artursuskevits <120181393+artursuskevits@users.noreply.github.com>
2023-08-30 11:48:37 +0300

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,1 +1,8 @@
-
+INSERT INTO room_type (description, max_capacity)
+VALUES ('cool room',1);
+INSERT INTO room_type (description, max_capacity)
+VALUES ('cool room',2);
+INSERT INTO room_type (description, max_capacity)
+VALUES ('cool room',3);
+ 
+select * from room_type
