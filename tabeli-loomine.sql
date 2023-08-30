93a1ffc6c2174f2358cff3d46c0ced8f8f84d2e1
artursuskevits <120181393+artursuskevits@users.noreply.github.com>
2023-08-30 11:22:51 +0300

    Update tabeli-loomine.sql

--- tabeli-loomine.sql
+++ tabeli-loomine.sql
@@ -1,1 +1,20 @@
+CREATE TABLE room_type(
+    room_typeID int PRIMARY KEY identity(1,1),
+    description varchar(80),
+    max_capacity int);
+select * from room_type
 
+CREATE TABLE guest(
+    guestID int PRIMARY KEY identity(1,1),
+    first_name varchar(80) ,
+	last_name varchar(80) Null ,
+    member_since date);
+ select * from guest
+
+ CREATE TABLE reservation(
+    reservationID int PRIMARY KEY identity(1,1),
+	date_in date,
+	date_out date,
+    made_by varchar(20) UNIQUE,
+    guestID int  FOREIGN KEY REFERENCES guest(guestID));
+ select * from reservation
