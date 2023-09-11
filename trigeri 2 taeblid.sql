46abbda84b48b7a2fdb24e2414f645d52fcc0090
artursuskevits <120181393+artursuskevits@users.noreply.github.com>
2023-09-11 10:21:10 +0300

    Update trigeri 2 taeblid.sql

--- trigeri 2 taeblid.sql
+++ trigeri 2 taeblid.sql
@@ -6,7 +6,13 @@ CREATE TABLE room_type(
     max_capacity int);
 select * from room_type
 
-
+CREATE TABLE room(
+    roomID int PRIMARY KEY identity(1,1),
+    number varchar(10) ,
+	status varchar(10) ,
+	name varchar(40) ,
+	smoke BIT,
+	room_typeID int Foreign Key References room_type(room_typeID));
 
 
 
