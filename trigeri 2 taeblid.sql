419a142be48f797985dd4b1901286660ff143f34
artursuskevits <120181393+artursuskevits@users.noreply.github.com>
2023-09-11 10:20:44 +0300

    Update trigeri 2 taeblid.sql

--- trigeri 2 taeblid.sql
+++ trigeri 2 taeblid.sql
@@ -1,5 +1,10 @@
 --SQL
 --Delaju na osnove room_type i room
+CREATE TABLE room_type(
+    room_typeID int PRIMARY KEY identity(1,1),
+    description varchar(80),
+    max_capacity int);
+select * from room_type
 
 
 
