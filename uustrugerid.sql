04d5f9b370dd19807454d6d30489cd96e08fa0e4
artursuskevits <120181393+artursuskevits@users.noreply.github.com>
2023-09-11 08:48:05 +0300

    Update uustrugerid.sql

--- uustrugerid.sql
+++ uustrugerid.sql
@@ -1,1 +1,20 @@
+Create table maakond(
+maakondid int primary key identity(1,1),
+maakond varchar(100)
 
+);
+
+Create table linn(
+linnid int primary key identity(1,1),
+linn varchar(100),
+maakondid int,
+foreign key (maakondid) references maakond(maakondid)
+);
+
+
+create table logi(
+id int primary key identity(1,1),
+andmed text,
+kuupaev datetime,
+kasutaja varchar(100)
+);
