7397693a127ef11cc6e8ede067241dc4b1d3714b
artursuskevits <120181393+artursuskevits@users.noreply.github.com>
2023-10-04 12:26:08 +0300

    Update ddltruggerid2.sql

--- ddltruggerid2.sql
+++ ddltruggerid2.sql
@@ -73,3 +73,13 @@ SELECT name,
        is_disabled
 FROM sys.triggers
 WHERE is_disabled = 0;
+
+SELECT 
+	t2.[name] TableTriggerReference
+	, SCHEMA_NAME(t2.[schema_id]) TableSchemaName
+	, t1.[name] TriggerName
+FROM sys.triggers t1
+	INNER JOIN sys.tables t2 ON t2.object_id = t1.parent_id
+WHERE t1.is_disabled = 0
+	AND t1.is_ms_shipped = 0
+	AND t1.parent_class = 1
